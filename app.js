56f56543c9952d1d218a2e8e6df11643e620e7dd
dianadeguzman30 <dianadeguzman329@gmail.com>
2022-10-21 15:37:57 +0800

    init

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const add = require('./add')
 const read = require('./read')
 const write = require('./write')
+const update = require('./update')
 
 
 const data = process.argv
+var note = {}
  
 if(data[2] == 'add') {
    
@@ -15,6 +17,7 @@ if(data[2] == 'add') {
     var oldNote = read()
 
     add(note, oldNote)
+    console.log(note, oldNote)
 
 
 }
@@ -23,4 +26,25 @@ if(data[2] == 'read') {
     const present = require('./present')
     
     present(read())
+}
+if(data[2] == 'delete') {
+    const id = data[3]
+    const oldNote = read()
+    const del = require('./del')
+    del(id, oldNote)
+    
+    console.log(del)
+  
+}
+if(data[2] == 'update')  {
+    const note = {
+      id: data[3],
+      title: data[4],
+      body: data[5]
+    }
+    const oldNote = read()
+
+    update(note,oldNote)
+
+    console.log(note, oldNote)
 }
